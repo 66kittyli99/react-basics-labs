0cc60413cb2d08bb9444ef6c65714a0f28d88f3b
Kitty Li <kittyli8113@gmail.com>
2025-01-29 16:06:20 +0000

    Added task state

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
 
@@ -14,21 +15,25 @@ function App() {
     <div className="container">
 
       <h1>Tasky</h1>
-      return (
-    <div className="container">
-      <h1>Tasky</h1>
-      <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
-      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
-      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
-    </div>
-  );
-
-
-        import React, { useState } from 'react';
-
+     
+   
+    {taskState.tasks.map((task) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+    />
+  ))} 
 
     </div>
   );
+
+
+      
+
+
+    
+
 }
 
 export default App;
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -4,8 +4,8 @@ const Task = (props) => {
     return (
         <div className="card">
             <p>{props.title}</p>
-            <p>Due: {props.deadline}</p>
-            <p>{props.description}</p>
+            <p className="deadline">Due: {props.deadline}</p>
+            <p className="description">{props.description}</p>
         </div>
     )
     
